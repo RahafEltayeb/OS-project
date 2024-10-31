bc276195153d41d571de3c21ec42e4b069ca7f5f
RahafEltayeb <re1801755@qu.edu.qa>
2024-10-31 12:26:10 +0300

    Update Search.sh

--- Search.sh
+++ Search.sh
@@ -16,5 +16,10 @@ echo "Number of files larger than 1MB: $file_count" >> "$output_file"
 
 if [ -s "$output_file" ] #Check if 'bigfile' is not empty
 then
-    mail -s "Files larger than 1MB found on VM2" re1801755@qu.edu.qa < "$output_file" #send email, QUID@qu.edu.qa
+    {
+      echo "Subject: Files larger than 1MB found on VM2"
+      echo "To: re1801755@qu.edu.qa"
+      echo ""  #Insert a blank line between headers and body
+      cat "$output_file"
+    } | msmtp --from=re1801755@qu.edu.qa -t re1801755@qu.edu.qa
 fi
